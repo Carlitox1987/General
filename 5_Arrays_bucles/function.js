b5d7204d3a7aafe6cb8a1ee12be32fee93cfebf5
carlos caballero <caballero.carlosadrian@gmail.com>
2022-03-21 20:27:54 -0300

    major updates:
    limpiar function completed
    start ordenar function start (html/js)

--- 5_Arrays_bucles/function.js
+++ 5_Arrays_bucles/function.js
@@ -9,7 +9,7 @@ function myfunction()
     let li = document.createElement("li");
 
 
-    li.appendChild(document.createTextNode(apellidos+","+nombres+"-"+nota));
+    li.appendChild(document.createTextNode(apellidos+","+nombres+","+nota));
     ul.appendChild(li);
 
     const lis = document.getElementById('nota_ul').getElementsByTagName('li');
@@ -19,14 +19,48 @@ function myfunction()
     for (let i = 0; i <= lis.length - 1; i++) {
         cadena=cadena+lis[i].textContent+"\n";
     }
-    alert(cadena);
+  //  alert(cadena);
 }
 function limpiar(){
+    let lis = document.getElementById('nota_ul');
+  //  let cadena=lis.children[1];//obtiene un registro sub i
+    let i=0;
+   while (lis.firstChild) {
+    lis.removeChild(lis.firstChild);
+  }
+}
+function ordenar(){
+    let personaSplit=[''];
+    let persona = {
+        nombre: '',
+        apellido: '',
+        nota: 0,
+        };
+    let personas=[];
     const lis = document.getElementById('nota_ul').getElementsByTagName('li');
-    alert("se va a limpar");
-    let cadena="";
-    // Loop through the NodeList object. 
+    const psOrden=document.querySelector('input[name="sort"]:checked').value;
+    let orden=1;
+    let cantNotas=0;
+
     for (let i = 0; i <= lis.length - 1; i++) {
-        lis.removeChild(lis.removeChild[i]);
+        personaSplit=lis[i].textContent.split(',');
+        persona.nombre=personaSplit[0];
+        persona.apellido=personaSplit[1];
+        persona.nota=personaSplit[2];
+        personas.push(persona);
     }
+    switch (psOrden) {
+        case 'Nombre':
+          orden=0;
+          break;
+        case 'Apellido':
+          orden=1;
+          break;
+        default:
+          orden=2;
+      }
+    cantNotas=personas.length;
+
+    alert(cantNotas);
+
 }
